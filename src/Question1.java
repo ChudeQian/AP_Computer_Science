e8ad1b8ff2d2bafaa0535e7687ec32cc7823d120
Chude Qian <frank_chude.qian@outlook.com>
2015-11-16 08:24:50 -0500

    This is for My AP Computer Science Homework! <Mr. D is cool!>
     Latest Update: 11/15/2015 @21:17
    
     Project Includes:
          magpie

--- src/Question1.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Qian on 11/15/2015.
- */
-public class Question1 {
-    public static void main(String args[]) {
-        System.out.println("The bug is for comparing the string directly using '=' instead of using the java equal.");
-    }
-}
