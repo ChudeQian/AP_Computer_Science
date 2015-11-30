3a051ffaa54fbf038b640e060e8710759d28adc8
Chude Qian <frank_chude.qian@outlook.com>
2015-11-30 17:54:32 -0500

    This is for My AP Computer Science Homework! <Mr. D is cool!>
     Latest Update: 11/30/2015@17:48
    
     This round is focused for my Chapter 8 test correction. They include four free response question.
    
     The class UserInputHandler is still under development.

---
+++ src/FR4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by Qian on 11/30/2015.
+ */
+public class FR4 {
+    public static void main(String args[]) {
+        System.out.println("Mr. D is going to hate me if he runs this. LOL");
+        System.out.println("请输入你要检测的单词： ");
+        Scanner 键盘识别器 = new Scanner(System.in);
+        String 用户输入 = 键盘识别器.nextLine();
+
+    }
+
+    public }
