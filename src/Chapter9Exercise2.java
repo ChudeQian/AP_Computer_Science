20bf1d78d17f5e198791af8c6c5a17508b0e7a4c
Frank <frank_chude.qian@outlook.com>
2016-01-09 18:32:50 -0500

    Clean for new Semester

--- src/Chapter9Exercise2.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Qian on 12/3/2015.
- */
-public class Chapter9Exercise2 {
-    //False
-    //False
-    //False
-    //True
-}
--- src/Chapter9Exercise6.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Qian on 12/3/2015.
- */
-public class Chapter9Exercise6 {
-    //it counts how many none zero term are there in the array
-}
