3ea176b6ee10feada463d2fdf91fb1024dc7157e
Chude Qian <frank_chude.qian@outlook.com>
2016-01-28 13:31:31 -0500

    This is for My AP Computer Science Homework! <Mr. D is cool!>
    Latest Update: 01/28/2016
    
    Chapter 11 Exercise-Shuffler

---
+++ src/LineList.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Qian on 1/28/2016.
+ */
+public class LineList {
+    private ArrayList<String> strings;
+
+    public LineList() {
+
+    }
+
+    public int size() {
+        int lineNumber;
+
+        return lineNumber;
+    }
+
+    public String get(int k) {
+
+    }
+
+    public void add(String line) {
+
+    }
+
+    public String remove(int k) {
+
+    }
+
+    public void move(int index, int newIndex) {
+
+    }
+
+    public void shuffle() {
+
+    }
+}
