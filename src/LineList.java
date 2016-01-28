c514a8522b2c4726ea67528920d75effd73c5649
Chude Qian <frank_chude.qian@outlook.com>
2016-01-28 13:47:43 -0500

    This is for My AP Computer Science Homework! <Mr. D is cool!>
    Latest Update: 01/28/2016
    
    Chapter 11 Exercise-Shuffler

--- src/LineList.java
+++ src/LineList.java
@@ -4,35 +4,50 @@ import java.util.ArrayList;
  * Created by Qian on 1/28/2016.
  */
 public class LineList {
-    private ArrayList<String> strings;
+    private ArrayList<String> stringList;
 
     public LineList() {
-
+        stringList = new ArrayList<>();
     }
 
     public int size() {
-        int lineNumber;
-
-        return lineNumber;
+        return stringList.size();
     }
 
     public String get(int k) {
-
+        return stringList.get(k);
     }
 
     public void add(String line) {
-
+        stringList.add(line);
     }
 
     public String remove(int k) {
-
+        String localTemp = stringList.get(k);
+        stringList.remove(stringList.get(k));
+        return localTemp;
     }
 
     public void move(int index, int newIndex) {
+        String localTemp = stringList.get(index);
+        if (index < newIndex) {
+            stringList.add(newIndex, localTemp);
+            stringList.remove(index);
+        } else {
+            stringList.add(newIndex, localTemp);
+            stringList.remove(index - 1);
+        }
 
     }
 
     public void shuffle() {
+        int n = stringList.size();
+        while (n >= 2) {
+            int randomN = (int) (Math.random() * n);
+            String local = stringList.get(randomN);
+            stringList.set(n, local);
+            n--;
+        }
 
     }
 }
