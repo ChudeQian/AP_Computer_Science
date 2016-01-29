b8c21ab341034eac705300cb2d48ecdd1793da2d
Chude Qian <frank_chude.qian@outlook.com>
2016-01-29 10:38:26 -0500

    This is for My AP Computer Science Homework! <Mr. D is cool!>
    Latest Update: 01/28/2016
    
    Chapter 11 Exercise-Indexing

--- src/DocumentIndex.java
+++ src/DocumentIndex.java
@@ -4,5 +4,15 @@ import java.util.ArrayList;
  * Created by Qian on 1/29/2016.
  */
 public class DocumentIndex extends ArrayList<IndexEntry> {
+    public DocumentIndex() {
+        ArrayList<Integer> numList = new ArrayList<>();
+    }
 
+    public DocumentIndex(int Number) {
+        ArrayList<Integer> numList = new ArrayList<>(Number);
+    }
+
+    public void addWord(String str, int num) {
+
+    }
 }
