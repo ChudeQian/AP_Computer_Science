277582d2e6384dae4aded30fd2291f860951a762
Frank <frank_chude.qian@outlook.com>
2016-02-18 10:07:56 -0500

    HomeWork for Chapter 12

--- src/Diploma.java
+++ src/Diploma.java
@@ -2,12 +2,15 @@
  * Created by frank on 2/16/2016.
  */
 public class Diploma {
-    private String name,item;
-    public Diploma (String name, String item){
-        this.name = name;
-        this.item = item;
+    private String name, subject;
+
+    public Diploma(String nm, String subj) {
+        this.name = nm;
+        this.subject = subj;
     }
-    public String toString(){
-        return "This certifies that "+name+"has completed a MOOC in "+item;
+
+    public String toString() {
+        return "This certifies that " + this.name + "\n" +
+                "has completed a course in " + this.subject;
     }
 }
--- src/DiplomaWithHonors.java
+++ src/DiplomaWithHonors.java
@@ -2,12 +2,11 @@
  * Created by frank on 2/16/2016.
  */
 public class DiplomaWithHonors extends Diploma {
-    public DiplomaWithHonors(String name, String item){
-        super(name,item);
-
+    public DiplomaWithHonors(String nm, String subj) {
+        super(nm, subj);
     }
-    public String toString(){
-        return "This certifies that "+name+"has completed a MOOC in "+item+"*** With Honors ***";
 
+    public String toString() {
+        return super.toString() + "\n*** with honors ***";
     }
 }
