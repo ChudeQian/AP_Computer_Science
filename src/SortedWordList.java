ef992c38aad8a7b078bb533e4c9510e6ce17e969
Frank <frank_chude.qian@outlook.com>
2016-02-23 09:18:46 -0500

    Chapter 14 Sort word list lab

--- src/SortedWordList.java
+++ src/SortedWordList.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 /**
  * Created by frank on 2/23/2016.
  */
-public class SortedWordList {
+public class SortedWordList extends ArrayList<String>{
+    public SortedWordList(){
 
+    }
+    public SortedWordList(int initalCapacity){
+
+    }
 }
