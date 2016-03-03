1261a620a149bba13e4348b03fa97b989d0a3f2e
Chude Qian <frank_chude.qian@outlook.com>
2016-03-03 14:26:18 -0500

    Chapter 14 Homework Questions

--- src/Chapter14QuadraticFunction.java
+++ src/Chapter14QuadraticFunction.java
@@ -2,18 +2,62 @@
  * Created by Qian on 3/1/2016.
  */
 public class Chapter14QuadraticFunction {
-    public Chapter14QuadraticFunction() {
+    private int a, b, c;
 
+    public Chapter14QuadraticFunction(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public static String getValue(int value) {
+        if (value > 0 && value != 1) {
+            return "+" + value;
+        } else if (value < 0) {
+            return "" + value;
+        } else return "";
     }
 
     public double valueAt(double x) {
-        return -1;
+        return (this.a * x * x + this.b * x + this.c);
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        if (this.a == 0) {
+            if (this.b == 0) {
+                return "" + this.c;
+            }
+            return "" + getValue(this.b) + "x" + getValue(this.c);
+        } else {
+            return "" + getValue(this.a) + "x^2" + getValue(this.b) + "x" + getValue(this.c);
+        }
     }
+
+    public boolean equals(Chapter14QuadraticFunction obj) {
+        return obj.a == this.a && obj.b == this.b && obj.c == this.c;
+    }
+
+    public int compareTo(Chapter14QuadraticFunction obj) {
+        if (obj.a > this.a) {
+            return -1;
+        } else if (obj.a < this.a) {
+            return 1;
+        } else {
+            if (obj.b > this.b) {
+                return -1;
+            } else if (obj.b < this.b) {
+                return 1;
+            } else {
+                if (obj.c > this.c) {
+                    return -1;
+                } else if (obj.c < this.c) {
+                    return 1;
+                } else return 0;
+            }
+        }
+    }
+
 
 
 }
---
+++ src/Chapter14QuadraticFunctionValueComparator.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Qian on 3/3/2016.
+ */
+public class Chapter14QuadraticFunctionValueComparator {
+    public Chapter14QuadraticFunctionValueComparator() {
+
+    }
+}
