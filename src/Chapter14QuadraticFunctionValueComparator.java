a4e297f7e71fe6ccb2b61a4ab6746dc05313ad3b
Frank <frank_chude.qian@outlook.com>
2016-03-03 14:36:43 -0500

    Chapter 14 Homework!!!

--- src/Chapter14QuadraticFunctionValueComparator.java
+++ src/Chapter14QuadraticFunctionValueComparator.java
@@ -2,7 +2,18 @@
  * Created by Qian on 3/3/2016.
  */
 public class Chapter14QuadraticFunctionValueComparator {
-    public Chapter14QuadraticFunctionValueComparator() {
 
+    double x;
+
+    public Chapter14QuadraticFunctionValueComparator() {
+        x = 0;
+    }
+
+    public Chapter14QuadraticFunctionValueComparator(double x) {
+        this.x = x;
+    }
+
+    public int compare(Chapter14QuadraticFunction o1, Chapter14QuadraticFunction o2) {
+        return (int) (o1.valueAt(x) - o2.valueAt(x));
     }
 }
