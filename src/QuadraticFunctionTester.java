39c2b9d5a3ec3a6d84a70f358a32516a920d5281
Frank <frank_chude.qian@outlook.com>
2016-03-27 23:46:46 -0400

    Clean Up

--- src/QuadraticFunctionTester.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Qian on 3/1/2016.
- */
-public class QuadraticFunctionTester {
-    public static void main(String args[]) {
-        System.out.println(new Chapter14QuadraticFunction(1, -5, 6));
-    }
-}
